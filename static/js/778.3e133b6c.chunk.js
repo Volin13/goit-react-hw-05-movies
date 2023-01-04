b3445a2bfed61121284403c91aa63bae5a2abeb6
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-01-04 16:01:00 +0000

    Deploying to gh-pages from @ Volin13/goit-react-hw-05-movies@7f3291e392293053704d01ad20f3014ae90c8e13 🚀

---
+++ static/js/778.3e133b6c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[778],{4778:function(n,e,t){t.r(e);var i,o=t(168),r=t(6444),a=t(184),c=r.ZP.div(i||(i=(0,o.Z)(["\n  font-size: 50px;\n  text-align: center;\n  margin: auto;\n  color: red;\n  margin-top: 50px;\n"])));e.default=function(){return(0,a.jsx)(c,{children:"404 Page not found."})}}}]);
+//# sourceMappingURL=778.3e133b6c.chunk.js.map
