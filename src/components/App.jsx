20f2e8227ed37a61b250fdb1b29c081b8d75c70b
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-01-04 20:42:23 +0200

    commit message3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { lazy, useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { Home, SharedLayout } from './index';
-
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-const MovieCredits = lazy(() => import('./MovieCredits/MovieCredits'));
-const Reviews = lazy(() => import('./Reviews/Reviews'));
-const NotFound = lazy(() => import('../pages/404/404'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,9 +11,20 @@ const queryClient = new QueryClient({
   },
 });
 
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const MovieCredits = lazy(() => import('./MovieCredits/MovieCredits'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const NotFound = lazy(() => import('../pages/404/404'));
+
 const App = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (pathname === '/goit-react-hw-05-movies') navigate('/');
+  }, [navigate, pathname]);
   return (
-    <div>
+    <>
       <QueryClientProvider client={queryClient}>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
@@ -33,7 +38,7 @@ const App = () => {
           </Route>
         </Routes>
       </QueryClientProvider>
-    </div>
+    </>
   );
 };
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
 export { default as Home } from '../pages/Home/Home';
 export { default as SharedLayout } from '../pages/SharedLayout/SharedLayout';
-// export { default as Trending } from './Trending/Trending';
-// export { default as SearchBar } from './SearchBar/SearchBar';
