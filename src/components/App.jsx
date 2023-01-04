fece52f8d13ec84f6bb8f92c71ba4a09064e72c6
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-01-04 17:19:26 +0200

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, SharedLayout } from './index';
+
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const CastInfo = lazy(() => import('./MovieCredits/MovieCredits'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const NotFound = lazy(() => import('../pages/404/404'));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<CastInfo />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 };
